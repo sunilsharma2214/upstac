7c1f1d2517aa9e56fcb6729d6eee600d0bc17649
sunilsharma2214 <sunilsharma.2214@gmail.com>
2021-04-27 16:55:28 +0530

    commit

---
+++ UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
@@ -0,0 +1,107 @@
+package org.upgrad.upstac.testrequests.consultation;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.upgrad.upstac.config.security.UserLoggedInService;
+import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.testrequests.TestRequest;
+import org.upgrad.upstac.testrequests.TestRequestQueryService;
+import org.upgrad.upstac.testrequests.TestRequestUpdateService;
+import org.upgrad.upstac.testrequests.flow.TestRequestFlowService;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+import static org.upgrad.upstac.exception.UpgradResponseStatusException.asBadRequest;
+import static org.upgrad.upstac.exception.UpgradResponseStatusException.asConstraintViolation;
+
+
+@RestController
+@RequestMapping("/api/consultations")
+public class ConsultationController {
+
+    Logger log = LoggerFactory.getLogger(ConsultationController.class);
+
+
+
+
+    @Autowired
+    private TestRequestUpdateService testRequestUpdateService;
+
+    @Autowired
+    private TestRequestQueryService testRequestQueryService;
+
+
+    @Autowired
+    TestRequestFlowService  testRequestFlowService;
+
+    @Autowired
+    private UserLoggedInService userLoggedInService;
+
+
+
+    @GetMapping("/in-queue")
+    @PreAuthorize("hasAnyRole('DOCTOR')")
+    public List<TestRequest> getForConsultations()  {
+       // Find the list of test request with status 'LAB_TEST_COMPLETED'
+       return testRequestQueryService.findBy(RequestStatus.LAB_TEST_COMPLETED);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('DOCTOR')")
+    public List<TestRequest> getForDoctor()  {
+       //Logged-in User
+	   User doctor = userLoggedInService.getLoggedInUser();
+	   
+	   //List of test assigned to user(doctor)
+	   return testRequestQueryService.findByDoctor(doctor);
+    }
+
+
+
+    @PreAuthorize("hasAnyRole('DOCTOR')")
+    @PutMapping("/assign/{id}")
+    public TestRequest assignForConsultation(@PathVariable Long id) {
+        try {
+          //Logged-in User
+	      User doctor = userLoggedInService.getLoggedInUser();
+	   
+	      //Returning the list of tests assigned to the user(doctor)
+		  TestRequest testRequest = testRequestUpdateService.assignForConsultation(id,doctor)
+	      return testRequest;
+    
+        }catch (AppException e) {
+            throw asBadRequest(e.getMessage());
+        }
+    }
+
+
+
+    @PreAuthorize("hasAnyRole('DOCTOR')")
+    @PutMapping("/update/{id}")
+    public TestRequest updateConsultation(@PathVariable Long id,@RequestBody CreateConsultationRequest testResult) {
+    try {
+           //Logged-in User
+	      User doctor = userLoggedInService.getLoggedInUser();
+	   
+	      //Update & return the consultation request
+		  TestRequest testRequest = testRequestUpdateService.updateConsultation(id,testResult,doctor)
+	      return testRequest;
+     
+          
+        } catch (ConstraintViolationException e) {
+            throw asConstraintViolation(e);
+        }catch (AppException e) {
+            throw asBadRequest(e.getMessage());
+        }
+    }
+
+
+
+}
